00e61722d28d87f96af20bd29b6f511c77c9d639
Sugandar <sugandar@ozzi.tech>
2021-06-03 19:20:09 +0530

    Readux added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { Layout, Seo } from "components"
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleDarkMode } from 'Redux/actions';
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi Developers</h1>
-    <p>Welcome to new Custom Gatsby Starter </p>
-    <p>
-      <Link to="/about-us/">Go to About Us</Link> <br />
-    </p>
-  </Layout>
-)
-
+const IndexPage = () => {
+    const dispatch = useDispatch();
+    const { isDarkMode } = useSelector((state: any) => {
+    return state.DarkModeReducer;
+  });
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi Developers</h1>
+      <p>Welcome to new Custom Gatsby Starter </p>
+      <button
+        type="button"
+        style={{backgroundColor: isDarkMode ? "black" : "white", color: isDarkMode ? "white" : "black"}}
+        onClick={() => {
+        dispatch(toggleDarkMode());
+      }}>Toggle Dark Mode </button>
+      <p>
+        <Link to="/about-us/">Go to About Us</Link> <br />
+      </p>
+    </Layout>
+  )
+}
 export default IndexPage
