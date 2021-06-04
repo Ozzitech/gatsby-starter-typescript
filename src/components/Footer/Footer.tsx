3c15794919e083115b23f06de95652a255ca9f44
Sugandar <sugandar@ozzi.tech>
2021-06-04 16:18:24 +0530

    file moved out from src dir

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
-import * as React from "react"
+import * as React from 'react';
 
 export const Footer = () => (
-    <footer
-        style={{
-            marginTop: `2rem`,
-        }}
-        >
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-    </footer>
-)
+  <footer
+    style={{
+      marginTop: `2rem`,
+    }}
+  >
+    © {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.gatsbyjs.com">Gatsby</a>
+  </footer>
+);
