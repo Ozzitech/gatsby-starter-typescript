36c8729908043b05a4a37dc28de3af023cffaae7
sugandar <sugandar@ozzi.tech>
2022-12-21 17:20:37 +0530

    Fix linting and testing error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 import { PageLayout } from 'components';
 
-const NotFoundPage = () => (
-  <PageLayout title="404: Not found">
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </PageLayout>
-);
+const NotFoundPage = () => {
+  return (
+    <PageLayout title="404: Not found">
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </PageLayout>
+  );
+};
 
 export default NotFoundPage;
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -3,12 +3,14 @@ import { Link } from 'gatsby';
 
 import { PageLayout } from 'components';
 
-const AboutUS = () => (
-  <PageLayout title="About Us">
-    <h1>About Us</h1>
-    <p>Welcome to About Us</p>
-    <Link to="/">Go back to the Homepage</Link>
-  </PageLayout>
-);
+const AboutUS = () => {
+  return (
+    <PageLayout title="About Us">
+      <h1>About Us</h1>
+      <p>Welcome to About Us</p>
+      <Link to="/">Go back to the Homepage</Link>
+    </PageLayout>
+  );
+};
 
 export default AboutUS;
